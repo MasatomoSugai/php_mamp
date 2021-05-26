ee6014a1cda559dd1928100c115a3c277c18f801
Masatomo Sugai <tech.masatomo.s@gmail.com>
2021-05-26 19:53:52 +0900

    initilize commit

---
+++ products.php
@@ -0,0 +1,12 @@
+<?php
+  require_once 'classes/product.php';
+
+
+  $products =[
+    new Product("avocado", "アボカド", 230, "./images/image_1.jpg"),
+    new Product("orange", "オレンジ",490, "./images/image_2.jpg"),
+    new Product("lemon", "レモン", 190, "./images/image_3.jpg"),
+    new Product("pineapple", "パイナップル", 980, "./images/image_4.jpg")
+    
+  ];
+?>
